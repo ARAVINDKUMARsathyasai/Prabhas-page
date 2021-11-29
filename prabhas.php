47ca714fc2bb4057ed6548ef3c39894398f4731f
ARAVINDKUMAR SATHYASAI BOBBA <74555212+ARAVINDKUMARsathyasai@users.noreply.github.com>
2021-11-29 06:08:34 -0800

    Add files via upload

---
+++ prabhas.php
@@ -0,0 +1,316 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>PRABHAS</title>
+<style type="text/css">
+    
+    body{
+        
+       background: url(back.jpg);
+    }
+    .prabha
+    {
+        background-color: darkblue;
+    }
+    .body{
+       background: url(home%20back.jpg);
+        text-align: center;
+        color: aqua;
+        font-family: algerian;
+        
+    }
+    html, body {
+                width: 100%;
+                height: 100%;
+                margin: 0;
+                padding: 0;
+               
+            }
+      #container {
+                width:auto;
+                height:inherit;
+                margin: 0;
+                padding: 0;
+               
+            }
+    h3
+    {
+        text-align: center;
+        font-family: algerian;
+        
+    }
+     form
+    {
+         
+         background-image:url(prabhu.jpg);
+          border-bottom-left-radius: 10px;
+         border-bottom-right-radius: 10px;
+         border-bottom-width: thick;
+            border-radius: 10px;
+         border-bottom-style: groove;
+         border-bottom-color: darkcyan;
+         border-left-style: groove;
+         border-left-color: darkcyan;
+         border-right-style: double;
+         border-right-color: hotpink;
+         border-top-style: groove;
+         border-top-color: darkcyan;
+            padding: 90px;
+            margin: 70px;
+            display: inline-block;
+            color:aliceblue;
+         
+           
+    }
+    h1{
+        background-color: aliceblue;
+        text-align: center;
+        font-size:30px;
+        color:darkblue;
+        font-family: algerian;
+        
+    }
+     h2
+    {
+        background-color: aliceblue;
+        border-style:groove;
+            border-radius: 15px;
+            padding: 2px;
+            margin: 2px;
+            font-size: 20px;
+            
+           
+        color:darkblue;
+        font-family: algerian;
+        
+    }
+    h3
+    {
+     color :aqua;
+     font-family: algerian;
+    text-align: left;
+        font-size: 25px;
+    }
+     a{text-decoration: none;}
+        a:link{color: deeppink  ;}
+        a:visited{color: deeppink;}
+        a:hover{color: blue;}
+        a:active{color: orangered}
+        
+        a{text-decoration: none;}
+        a:link{color:deeppink;}
+        a:visited{color: deeppink;}
+        a:visited{color: deeppink;}
+        a:hover{color: blue;}
+        a:active{color: orangered}
+   
+    h10
+    {
+      
+        background-clip: padding-box; 
+        color:aqua;
+        font-family: algerian;
+        font-size:25px;
+        text-align: left;
+    }
+    
+.scroll-left 
+    {
+ height: 50px;	
+ overflow: hidden;
+ position: relative;
+}
+.marquee {
+
+  margin: 0 auto;
+  white-space: nowrap;
+  overflow: hidden;
+  position: absolute;
+}
+
+.marquee span {
+  font-family: algerian;
+    font-size: 30px;
+    color: darkred;
+    display: inline-block;
+  padding-left: 100%;
+  animation: marquee 25s linear infinite;
+}
+
+.marquee2 span {
+  animation-delay: 2.5s;
+}
+
+@keyframes marquee {
+  0% {
+    transform: translate(0, 0);
+  }
+  100% {
+    transform: translate(-100%, 0);
+  }
+}
+    .marquee{
+        background-color:gold;
+    }
+    option
+    {
+        font-family: algerian;
+        font-size: 20px;
+       
+        background-color: blue;
+         color:gold;
+    }
+
+    </style>
+
+
+
+</head>
+<body >
+    
+<div id="container">
+    
+  <div class="prabha">  <h1><b>WELCOME REBAL FANS</b></h1>
+
+    </div>
+    
+    <div class="body">
+        <p class="marquee">
+  <span>WELCOME USERS..... WELCOME USERS..... WELCOME USERS.....  WELCOME USERS.....  WELCOME USERS.....   &nbsp;</span>
+</p>
+<form method="POST" action="file:///C:/Users/bobba/Desktop/web%20page/prabhasin.html">
+    <b><h3>FULL NAME:</h3></b>
+    <input type='text' name='funame' id='name' value='' placeholder="enter name" maxlength="30" required /><br/>
+	 
+    <b><h10><br/><b>FAN OFF PRABHAS</b><br/></h10></b>
+     &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<b><h10><input type="radio" name ="fname" />YES</h10></b> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+    <b><h10><input type="radio" name ="fname" id ='name ' />NO:<select name="FAN OFF" size="1">
+<option  value="all" selected="selected">SELECT</option>
+<option value="all" >I LOVE ALL ACTORS!</option>
+<option value="AADI">AADI</option>
+<option value="AKKINENI AKHIL">AKKINENI AKHIL</option>
+<option value="AKKINENI NAGESHWARA RAO">AKKINENI NAGESHWARA RAO</option>
+<option value="ALLARI NARESH">ALLARI NARESH</option>
+<option value="ALLU ARJUN">ALLU ARJUN</option>
+<option value="BALA KRISHNA">BALA KRISHNA</option>
+<option value="CHIRANJEEVI">CHIRANJEEVI</option>
+<option value="Dr. RAJASEKHAR">Dr. RAJASEKHAR</option>
+<option value="GOPICHAND">GOPICHAND</option>
+<option value="Jr. N.T.R">Jr. N.T.R</option>
+<option value="KAMAL HAASAN">KAMAL HAASAN</option>
+<option value="KRISHNA">KRISHNA</option>
+<option value="MAHESH BABU">MAHESH BABU</option>
+<option value="MANOJ MANCHU">MANOJ MANCHU</option>
+<option value="NAGA CHAITANYA">NAGA CHAITANYA</option>
+<option value="NAGARJUNA">NAGARJUNA</option>
+<option value="NANDAMURI KALYAN RAM">NANDAMURI KALYAN RAM</option>
+<option value="NANI">NANI</option>
+<option value="N.T. RAMA RAO">N.T. RAMA RAO</option>
+<option value="NITIN REDDY">NITIN REDDY</option>
+<option value="NITHIN">NITHIN</option>
+<option value="NIKIL">NIKIL</option>
+<option value="PAWAN KALYAN">PAWAN KALYAN</option>
+<option value="RAJENDRA PRASAD">RAJENDRA PRASAD</option>
+<option value="MANOJ MANCHU">MANOJ MANCHU</option>
+<option value="NAGA CHAITANYA">NAGA CHAITANYA</option>
+<option value="NAGARJUNA">NAGARJUNA</option>
+<option value="NANDAMURI KALYAN RAM">NANDAMURI KALYAN RAM</option>
+<option value="RAJNIKANTH">RAJNIKANTH</option>                
+<option value="RAJ TARUN">RAJ TARUN</option>
+<option value="RAM CHARAN TEJA">RAM CHARAN TEJA</option>
+<option value="RAM POTHINENI">RAM POTHINENI</option>
+<option value="RANA DAGGUBATI">RANA DAGGUBATI</option>
+<option value="RAVI TEJA">RAVI TEJA</option>
+<option value="SAI DHARAM TEJ">SAI DHARAM TEJ</option>
+<option value="SHARWANAND">SHARWANAND</option>
+<option value="SHOBAN BABU">SHOBAN BABU</option>
+<option value="SIDDHARTH">SIDDHARTH</option>
+<option value="SUNIL">SUNIL</option>
+<option value="SURIYA">SURIYA</option>
+<option value="S.V. RANGA RAO">S.V. RANGA RAO</option>
+<option value="UDAY KIRAN">UDAY KIRAN</option>
+<option value="VICTORY VENKATESH">VICTORY VENKATESH</option>
+<option value="VIJAY">VIJAY</option>
+
+</select></h10><br/></b>
+     <B><h10>MOVIE YOU LIKE :-</h10></B>&nbsp; &nbsp; <select name="MOVIE YOU LIKE" size="1" >
+<option  value="all" selected="selected">SELECT</option>
+<option value="all" >I LOVE ALL MOVIES!</option>
+<option value="SAAHO">SAAHO</option>
+<option value="BAAHUBALI 2: THE CONCLUSION">BAAHUBALI 2: THE CONCLUSION</option>
+<option value="BAAHUBALI 1: THE BEGINNING">BAAHUBALI 1: THE BEGINNING</option>
+<option value="MIRCHI">MIRCHI</option>
+<option value="REBEL">REBEL</option>
+<option value="BILLA">BILLA</option>
+<option value="Mr PERFECT">Mr PERFECT</option>
+<option value="DARLING">DARLING</option>
+<option value="EK NIRANJAN">EK NIRANJAN</option>
+<option value="BUJJIGADU">BUJJIGADU</option>
+<option value="MUNNA">MUNNA</option>
+<option value="YOGI">YOGI</option>
+<option value="POURNAMI">POURNAMI</option>
+<option value="CHATRAPATHI">CHATRAPATHI</option>
+<option value="CHAKRAM">CHAKRAM</option>
+<option value="NAGARJUNA">VARSHAM</option>
+<option value="ADAVI RAMUDU">ADAVI RAMUDU</option>
+<option value="RAGHAVENDRA">RAGHAVENDRA</option>
+<option value="EESWAR">EESWAR</option>
+    </select>
+    
+    <b><h3>MOBILE No:</h3></b>
+  
+ <input type="tel" name="mob" id='number' value='' maxlength='10'placeholder="enter mobile number" pattern="[0-9]{2}[0-9]{4}[0-9]{4}" required>
+    <b><h3>Email Address:</h3></b>
+    <b><input type='email' name='email' id='email'placeholder="enter gmail" value='' maxlength="50" required /></b><br/>
+   
+        <b><input type="submit" value="submit"></b>
+<br/><br/>
+<h2>DEVELOPED BY @ PBS</h2>
+   
+   
+        </form>
+    
+    <p class="marquee">
+        <span> THANK YOU FOR SUBSCRIBING........!  THANK YOU FOR SUBSCRIBING........!   THANK YOU FOR SUBSCRIBING........!   &nbsp;</span>
+        </p>
+    </div>
+        </div>
+
+    </body>
+</html>
+    
+
+    <?php
+$con = mysql_connect("localhost","root","");
+if (!$con)
+  {
+  die('Could not connect: ' . mysql_error());
+  }
+  mysql_select_db("prabha", $con);
+  
+  
+
+ if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+  if(isset($_POST['submitpost'])){
+
+  $sql="INSERT INTO cvquery1 (FULL NAME, FAN OF, MOBILE No, Email Address)
+VALUES
+('$_POST[funame]','$_POST[fname]','$_POST[mob]','$_POST[email])";
+ 
+if (!mysql_query($sql,$con))
+  {
+  die('Error: ' . mysql_error());
+  }
+echo "<script>alert('Thank you for Submitting the details. Click OK to Continue');</script>";
+
+    }
+	
+
+}
+mysql_close($con);	
+   ?>
+   
+    </body>
+</html>
+    
